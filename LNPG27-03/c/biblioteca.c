1f68e67037ce73e5dabadf5699b40d9bd6ad997b
ZeNeto1 <152450111+ZeNeto1@users.noreply.github.com>
2024-03-27 21:30:01 -0300

    Add files via upload

---
+++ LNPG27-03/c/biblioteca.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char nomelivro[250];
+    char nomeautor[250];
+    int ano;
+    int CopiasDisponiveis;
+
+} biblioteca;
+
+void EmprestarLivro(){
+}
+void DevolverLivro(){
+}
+int ChecarCopias(){
+}
+void InformacaoLivro(){
+}
+
+int main(){
+
+
+return 0;
+}
---
+++ LNPG27-03/java/biblioteca.java
@@ -0,0 +1,27 @@
+
+public class biblioteca {
+    private String titulo;
+    private String autor;
+    private int ano;
+    private int copiadisponivel;
+
+    public biblioteca(String titulo, String autor, int ano, int copiadisponivel){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.copiadisponivel = copiadisponivel;
+
+    }
+    public void EmprestarLivro() {
+    }
+    public void DevolverLivro() {
+    }
+    public void VerificarCopias(){
+    }
+    public void InformacaoLivros(){
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
